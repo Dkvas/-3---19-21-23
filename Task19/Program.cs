20ed27e7890c64c5715971a769e6c0af9efa6ab3
Dmitry Kvasov <kvasof.d@gmail.com>
2022-10-07 16:39:29 +0300

    решено 2 задачи из 3

---
+++ Task19/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+using static System.Console;
+Clear();
+WriteLine("Вводим число: ");
+int num1 = Convert.ToInt32(ReadLine());
+int count = 0;
+while (num1 > 0) 
+{
+    count = count * 10 + num1 % 10;
+    num1 /= 10;
+}
+int num2 = count;
+if(num1 == num2)
+{
+    WriteLine($"Число {num1} палиандром.");
+}
+else
+{
+    WriteLine($"Число {num1} НЕ палиандром.");
+}
---
+++ Task23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+using static System.Console;
+Clear();
+WriteLine("Введите число: ");
+int N = Convert.ToInt32(ReadLine());
+int count = 1;
+while(count <= N)
+{
+    Write ($"{Math.Pow(count,3)}   ");
+    count++; 
+}
